cdf30ca9c84aebd03034e28b72981588ce1f7ae7
Noah Veenstra <veenstranoah@gmail.com>
2022-03-25 21:35:22 -0400

    mobile friendly changes!

---
+++ mobile-menu.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    document.querySelector('.menu').addEventListener("click", function () {
+        document.querySelector(".nav-fullscreen").classList.toggle("nav-fullscreen--open");
+        // document.body.style.overflowY = "hidden";
+    });
+
+    document.querySelector('.menu opened').addEventListener("click", function () {
+        document.querySelector(".nav-fullscreen").classList.toggle("nav-fullscreen--open");
+        // document.body.style.overflowY = "visible";
+        document.body.ad
+    });
+});
